3469bef8a800ea1dc4be42ad837bff0bf22b6442
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2024-10-11 22:00:06 +0330

    .

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import "./style.css";
+import "./style.scss";
 import "vazirmatn/misc/Non-Latin/Vazirmatn-NL-font-face.css";
 
 export default {
